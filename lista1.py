2373c8bb24aaabc5a5d28a696323fdbb1933dadb
Levente1998 <sze4szalev@vasvari.org>
2024-10-02 19:17:46 +0200

    Add files via upload

---
+++ lista1.py
@@ -0,0 +1,6 @@
+jegy = [1 , 2 , 3 , 4 , 5]
+oszt = ['9/ny' , '10/b' , '11/c' , '12/b haladó' , '1/13b' , '2/14b']
+nev = ['Ákos' , 'Dénes' , 'Laci' , 'Levente' , 'Nelli' , 'Regina'] 
+print(jegy)
+print(oszt)
+print(nev)
---
+++ lista3.py
@@ -0,0 +1,8 @@
+jegy = [1 , 2 , 3 , 4 , 5]
+oszt = ['9/ny' , '10/b' , '11/c' , '12/b haladó' , '1/13b' , '2/14b']
+nev = ['Ákos' , 'Dénes' , 'Laci' , 'Levente' , 'Nelli' , 'Regina']
+
+print(len(jegy))
+
+for x in jegy:
+    print(x , end=' ')
